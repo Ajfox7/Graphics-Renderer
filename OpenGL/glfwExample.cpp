8492172b8ba9f734a44440fa760930bce1ee79eb
Andrew Fox <fox00354@d.umn.edu>
2026-03-19 11:41:15 -0500

    OpenGL

---
+++ OpenGL/glfwExample.cpp
@@ -0,0 +1,169 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+#include "GLSL.h"
+
+int CheckGLErrors(const char *s)
+{
+    int errCount = 0;
+    return errCount;
+}
+
+int main(void)
+{
+    /* Initialize the library */
+    if (!glfwInit()) {
+        exit (-1);
+    }
+    // throw std::runtime_error("Error! initialization of glfw failed!");
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    /* Create a windowed mode window and its OpenGL context */
+    int winWidth = 1000;
+    float aspectRatio = 1.0; // 16.0 / 9.0; // winWidth / (float)winHeight;
+    int winHeight = winWidth / aspectRatio;
+    
+    GLFWwindow* window = glfwCreateWindow(winWidth, winHeight, "GLFW Example", NULL, NULL);
+    if (!window) {
+        std::cerr << "GLFW did not create a window!" << std::endl;
+        
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    GLenum err=glewInit();
+    if(err != GLEW_OK) {
+        std::cerr <<"GLEW Error! glewInit failed, exiting."<< std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    const GLubyte* renderer = glGetString (GL_RENDERER);
+    const GLubyte* version = glGetString (GL_VERSION);
+    std::cout << "Renderer: " << renderer << std::endl;
+    std::cout << "OpenGL version supported: " << version << std::endl;
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+    glClearColor(1.0, 0.7, 1.0, 1.0);
+
+    int fb_width, fb_height;
+    glfwGetFramebufferSize(window, &fb_width, &fb_height);
+    glViewport(0, 0, fb_width, fb_height);
+
+    // Need to set a projection matrix that fits the aspect ratio set
+    // by the window frame.
+    //
+    // The ortho parameters, in order: left, right, bottom, top, zNear, zFar
+    float halfWidth = 15.0 / 2.0;
+    float halfHeight = halfWidth / aspectRatio;
+    glm::mat4 projectionMatrix = glm::ortho(-halfWidth, halfWidth, -halfHeight, halfHeight, -10.0f, 10.0f);
+
+    GLint major_version;
+    glGetIntegerv(GL_MAJOR_VERSION, &major_version);
+    std::cout << "GL_MAJOR_VERSION: " << major_version << std::endl;
+
+    //Initialize all my data and get it on the GPU
+
+    GLuint m_triangleVBO[1], m_VAO;
+    sivelab::GLSLObject shader;
+
+    // create a Vertex Array Buffer to hold our triangle data                                               
+    glGenBuffers(1, m_triangleVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, m_triangleVBO[0]);
+
+    // this is the actual triangle data that will be copied to                                              
+    // the GPU memory                                                                                       
+    std::vector< float > host_VertexBuffer{ 
+        //position          //color
+        -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f,                                     
+        0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,                                      
+        0.0f, 0.5f, 0.0f,   0.0f, 0.0f, 0.0f
+    };                                     
+
+    int numBytes = host_VertexBuffer.size() * sizeof(float);
+
+    // copy the numBytes from host_VertexBuffer to the GPU and store in                                      
+    // the currently bound VBO                                                                              
+    glBufferData(GL_ARRAY_BUFFER, numBytes, host_VertexBuffer.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // once copied, we no longer need the data on the host                                                  
+    host_VertexBuffer.clear();
+
+    // create a vertex array object that will map the attributes in                                         
+    // our vertex buffer to different location attributes for our                                           
+    // shaders                                                                                              
+    glGenVertexArrays(1, &m_VAO);
+    glBindVertexArray(m_VAO);
+
+    // VAO details here       
+    // Attribute 0: position                                                                      
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, m_triangleVBO[0]);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+
+    // Attribute 1: color
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+
+    glBindVertexArray(0);
+
+    // Create a shader using my GLSLObject class                                                            
+    shader.addShader( "OpenGL/Debug/vertexShader_passthrough.glsl", sivelab::GLSLObject::VERTEX_SHADER );
+    shader.addShader( "OpenGL/Debug/fragmentShader_passthrough.glsl", sivelab::GLSLObject::FRAGMENT_SHADER );
+    shader.createProgram();
+
+    double timeDiff = 0.0, startFrameTime = 0.0, endFrameTime = 0.0;
+    
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        endFrameTime = glfwGetTime();
+        timeDiff = endFrameTime - startFrameTime;
+        startFrameTime = glfwGetTime();
+
+        // Clear the window's buffer (or clear the screen to our
+        // background color)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        /* Render your objects here */
+        shader.activate();
+        glBindVertexArray(m_VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+        shader.deactivate();
+
+        // Swap the front and back buffers
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+
+        if (glfwGetKey( window, GLFW_KEY_T ) == GLFW_PRESS) {
+            std::cout << "fps: " << 1.0/timeDiff << std::endl;
+        }
+        if (glfwGetKey( window, GLFW_KEY_ESCAPE ) == GLFW_PRESS) {
+            glfwSetWindowShouldClose(window, 1);
+        }
+    }
+  
+    glfwTerminate();
+    return 0;
+}
